79f0c56089fc4b3495471ad9675ac21fd3f32a42
calullmercade@gmail.com <calullmercade@gmail.com>
2021-10-05 14:51:02 +0200

    hello dungeon crawl

---
+++ src/map.rs
@@ -0,0 +1,72 @@
+use crate::prelude::*;
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Map {
+        Map {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+        ctx.set_active_console(0);
+        for y in camera.top_y..camera.bottom_y {
+            for x in camera.left_x..camera.right_x {
+                if self.in_bounds(Point::new(x, y)) {
+                    let idx = map_idx(x, y);
+                    match self.tiles[idx] {
+                        TileType::Floor => {
+                            ctx.set(
+                                x - camera.left_x,
+                                y - camera.top_y,
+                                WHITE, 
+                                BLACK, 
+                                to_cp437('.'),
+                            );
+                        }
+                        TileType::Wall => {
+                            ctx.set(
+                                x - camera.left_x,
+                                y - camera.top_y,
+                                WHITE, 
+                                BLACK, 
+                                to_cp437('#'),
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn in_bounds(&self, p: Point) -> bool {
+        let index = map_idx(p.x, p.y);
+        index < self.tiles.len()
+    }
+
+    pub fn can_enter_tile(&self, p: Point) -> bool {
+        self.in_bounds(p) && self.tiles[map_idx(p.x, p.y)] == TileType::Floor
+    }
+
+    pub fn try_idx(&self, p: Point) -> Option<usize> {
+        if !self.in_bounds(p) {
+            None
+        } else {
+            Some(map_idx(p.x, p.y))
+        }
+    }
+}
+
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
